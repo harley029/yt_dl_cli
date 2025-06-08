7bd483586395b5b2bc4b31b64a849e18e0b214f1
Oleksandr Kharchenko <harley029@gmail.com>
2025-06-08 23:00:20 +0200

    Docs added

---
+++ docs/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+
+import os
+import sys
+
+# Добавляем src/ в PYTHONPATH, чтобы Sphinx видел пакеты как yt_dl_cli.*
+sys.path.insert(0, os.path.abspath("../src"))
+
+project = "YT-DL-CLI Video downloader"
+copyright = "2025, Oleksandr Kharchenko"
+author = "Oleksandr Kharchenko"
+
+# Расширения Sphinx
+extensions = [
+    "sphinx.ext.autodoc",
+]
+
+# Пути для шаблонов
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# Опции для автодока по умолчанию
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+}
+
+
+# Тема HTML
+html_theme = "nature"
+html_static_path = ["_static"]
+def setup(app):
+    app.add_css_file("custom.css")
