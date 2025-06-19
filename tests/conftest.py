ac421b4cc8b03333708995e241b4107bb8079ac3
Oleksandr Kharchenko <155960725+harley029@users.noreply.github.com>
2025-06-19 08:17:31 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,17 @@
 """ Tests global settings  """
 import warnings
+import pytest
+from pathlib import Path
 
 warnings.filterwarnings(
     "ignore", category=RuntimeWarning, message="coroutine '.*' was never awaited"
 )
+
+@pytest.fixture(autouse=True, scope="session")
+def ensure_links_txt():
+    path = Path("links.txt")
+    if not path.exists():
+        path.write_text("https://test.url/should/be/ignored\n")
+    yield
+    if path.exists():
+        path.unlink()
